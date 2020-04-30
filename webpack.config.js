817960a92ecc9fa178b955f35266db8d2bb4f92b
amihut <amihut@EN610723>
2020-04-30 09:40:33 +0300

    set up babel-loader

--- webpack.config.js
+++ webpack.config.js
@@ -5,4 +5,13 @@ module.exports = {
     path: path.join(__dirname, 'public'),
     filename: 'bundle.js',
   },
+  module: {
+    rules: [
+      {
+        loader: 'babel-loader',
+        test: /\.js$/,
+        exclude: /node_modules/,
+      },
+    ],
+  },
 };
